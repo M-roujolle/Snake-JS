b4eba4f34d0348270d43afd88fc5c995e7770310
M-roujolle <you@example.com>
2022-04-27 10:02:52 +0200

    Declaration variable

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    const canvasWidth = 900;
+    const canvasHeight = 600;
+    const blockSize = 30;
+    const ctx;
+    const delay = 100;
+    const xCoord = 0;
+    const yCoord = 0;
+    const snakee;
+    const applee;
+    const widthInBlocks = canvasWidth/blockSize;
+    const heightInBlocks = canvasHeight/blockSize;
+    let score;
+    let timeOut;
+
+    init();
+}
